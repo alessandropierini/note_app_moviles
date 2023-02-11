242c759c4c569b1e88eec23248f72fdd37a57399
alessandropierini <99676715+alessandropierini@users.noreply.github.com>
2023-02-11 12:54:19 -0400

    bcrypt implementado

--- login/api/UserRoutes.js
+++ login/api/UserRoutes.js
@@ -1,15 +1,23 @@
 const router = require('express').Router()
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
 
 router.post('/register', async (req, res) => {
+
+    const newUser = new User(req.body)
+    console.log("Unencripted password: " + newUser.password)
+    bcrypt.hash(newUser.password, 10, function (err, hash) {
+        newUser.password = hash
+        console.log("encrypted password: " + newUser.password)
+    })
+
     if (await userExists(req.body.username)) {
         res.status(409).json({ error: 'Username already exists' })
         console.log('User already exists')
     } else {
-        const newUser = new User(req.body)
         newUser.save().then(user => {
             res.status(201).json(user)
-            console.log('User created')
         }).catch(err => {
             res.status(500).json({ error: err.message })
         })
@@ -17,26 +25,30 @@ router.post('/register', async (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-    User.findOne({username:req.body.username, password:req.body.password}).then(user => {
-        if(user){
-            res.status(200).json(user)
-        } else {
-            res.status(401).json({error: "Usuario o contrasena Incorrecta"})
-        }
-    }).catch(err => {
-        res.status(500).json({error: err.message})
+
+    User.findOne({ username: req.body.username }).then(user => {
+
+        bcrypt.compare(req.body.password, user.password, function (error, result) {
+
+            if (result) {
+                res.status(200).json(user)
+            } else {
+                console.log(result)
+                res.status(401).json({ error: "Usuario o contrasena Incorrecta" })
+            }
+        })
     })
 })
 
-const userExists = async (username) => {
-    const user = await User.findOne({ username: username})
-        if (user) {
-            return true
-        } else {
-            return false
-        }
-}
 
+const userExists = async (username) => {
+    const user = await User.findOne({ username: username })
+    if (user) {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 module.exports = router
--- login/models/user.js
+++ login/models/user.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 
 const UserSchema = new mongoose.Schema({
     username: {
@@ -16,16 +15,6 @@ const UserSchema = new mongoose.Schema({
     }
 })
 
-//doesnt encrypt password
-
-UserSchema.methods.encryptPassword = async (password) => {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
-  };
-  
-  UserSchema.methods.matchPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-  };
 
 
 module.exports = mongoose.model('User', UserSchema)
